5c1bd38f338a63356ced825f5110c66457d75b5d
zebra76966 <rs288442@gmail.com>
2024-12-03 15:47:54 +0530

    Added experimental abienta light and camera

--- src/App.js
+++ src/App.js
@@ -130,40 +130,144 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Sensor Data</h1>
+      <h1 className="display-6 fw-bold text-center">Sensor Tests</h1>
 
-      <h2>Motion</h2>
-      <p>
-        Acceleration (m/s²): <br />
-        X: {motionData.acceleration.x.toFixed(2)}, Y: {motionData.acceleration.y.toFixed(2)}, Z: {motionData.acceleration.z.toFixed(2)}
-      </p>
-      <p>
-        Rotation Rate (deg/s): <br />
-        Alpha: {motionData.rotationRate.alpha.toFixed(2)}, Beta: {motionData.rotationRate.beta.toFixed(2)}, Gamma: {motionData.rotationRate.gamma.toFixed(2)}
-      </p>
+      <div className="container-fluid mt-4">
+        <h2 className="display-6 fw-bold text-start text-muted">Motion</h2>
+        <div className="row">
+          <div className="col-12">
+            <div class="card text-white bg-primary mb-3 w-100 shadow ">
+              <div class="card-header fw-bold d-flex align-items-center justify-content-between ">
+                Acceleration (m/s²)
+                <i className="fa fa-tachometer"></i>
+              </div>
+              <div class="card-body">
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100">X:</h5>
+                  <h5 class="card-titled w-100">{motionData.acceleration.x.toFixed(2)}</h5>
+                </div>
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100">Y:</h5>
+                  <h5 class="card-titled w-100">{motionData.acceleration.y.toFixed(2)}</h5>
+                </div>
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100">Z:</h5>
+                  <h5 class="card-titled w-100">{motionData.acceleration.z.toFixed(2)}</h5>
+                </div>
+              </div>
+            </div>
+          </div>
 
-      <h2>Orientation</h2>
-      <p>
-        Alpha: {orientationData.alpha.toFixed(2)}, Beta: {orientationData.beta.toFixed(2)}, Gamma: {orientationData.gamma.toFixed(2)}
-      </p>
+          <div className="col-12">
+            <div class="card text-white bg-dark mb-3 w-100 shadow ">
+              <div class="card-header fw-bold d-flex align-items-center justify-content-between ">
+                Rotation Rate (deg/s)
+                <i className="fa fa-rotate-left"></i>
+              </div>
+              <div class="card-body">
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100">Alpha:</h5>
+                  <h5 class="card-titled w-100">{motionData.rotationRate.alpha.toFixed(2)}</h5>
+                </div>
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100">Beta:</h5>
+                  <h5 class="card-titled w-100"> {motionData.rotationRate.beta.toFixed(2)}</h5>
+                </div>
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100">Gamma:</h5>
+                  <h5 class="card-titled w-100">{motionData.rotationRate.gamma.toFixed(2)}</h5>
+                </div>
+              </div>
+            </div>
+          </div>
 
-      <h2>Location</h2>
-      <p>
-        Latitude: {location.latitude?.toFixed(2) || "Fetching..."} <br />
-        Longitude: {location.longitude?.toFixed(2) || "Fetching..."}
-      </p>
+          <div className="col-12">
+            <div class="card text-white bg-warning mb-3 w-100 shadow ">
+              <div class="card-header fw-bold d-flex align-items-center justify-content-between ">
+                Orientation(deg/s)
+                <i className="fa fa-ellipsis-v"></i>
+              </div>
+              <div class="card-body">
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100"> Alpha:</h5>
+                  <h5 class="card-titled w-100"> {orientationData.alpha.toFixed(2)}</h5>
+                </div>
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100"> Beta:</h5>
+                  <h5 class="card-titled w-100"> {orientationData.beta.toFixed(2)}</h5>
+                </div>
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100"> Gamma:</h5>
+                  <h5 class="card-titled w-100"> {orientationData.gamma.toFixed(2)}</h5>
+                </div>
+              </div>
+            </div>
+          </div>
 
-      <h2>Ambient Light</h2>
-      <p>Illuminance: {ambientLight !== null ? `${ambientLight} lux` : "Fetching..."}</p>
+          <div className="col-12">
+            <div class="card text-white bg-info mb-3 w-100 shadow ">
+              <div class="card-header fw-bold d-flex align-items-center justify-content-between ">
+                Location
+                <i className="fa fa-location-arrow"></i>
+              </div>
+              <div class="card-body">
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100"> Latitude: </h5>
+                  <h5 class="card-titled w-100"> {location.latitude?.toFixed(2) || "Fetching..."}</h5>
+                </div>
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100"> Longitude: </h5>
+                  <h5 class="card-titled w-100"> {location.longitude?.toFixed(2) || "Fetching..."}</h5>
+                </div>
+              </div>
+            </div>
+          </div>
 
-      <h2>Camera</h2>
-      <input type="file" accept="image/*" onChange={handleImageUpload} />
-      {selectedImage && (
-        <div>
-          <h3>Uploaded Image:</h3>
-          <img src={selectedImage} alt="Uploaded Preview" style={{ width: "300px", marginTop: "10px" }} />
+          <div className="col-12">
+            <div class="card text-white  mb-3 w-100 shadow " style={{ background: "coral" }}>
+              <div class="card-header fw-bold d-flex align-items-center justify-content-between ">
+                Ambient Light
+                <i className="fa fa-sun-o"></i>
+              </div>
+              <div class="card-body">
+                <div className="w-100 d-flex align-items-center justify-content-between">
+                  <h5 class="card-titled w-100"> Illuminance: </h5>
+                  <h5 class="card-titled w-100"> {ambientLight !== null ? `${ambientLight} lux` : "Fetching..."}</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12">
+            {selectedImage && (
+              <div className="my-2">
+                <h3 className="text-muted fs-3 fw-bold">Uploaded Image:</h3>
+                <img src={selectedImage} className="uploaded" alt="Uploaded Preview" style={{ width: "300px", marginTop: "10px" }} />
+              </div>
+            )}
+
+            <div class="card text-white bg-dark mb-3 w-100 shadow ">
+              <div class="card-header fw-bold d-flex align-items-center justify-content-between ">
+                Camera
+                <i className="fa fa-tachometer"></i>
+              </div>
+              <div class="card-body">
+                <div className="w-100 ">
+                  <label for="file" class="custum-file-upload">
+                    <div class="icon display-2">
+                      <i className="fa fa-camera-retro" />
+                    </div>
+                    <div class="text">
+                      <span>Click to upload image</span>
+                    </div>
+                    <input id="file" type="file" accept="image/*" onChange={handleImageUpload} />
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
